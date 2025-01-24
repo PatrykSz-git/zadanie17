c7cd0ec659e52f1c8f1c79ebad91818b9c61b47d
PatrykSz-git <sztrauch53@gmail.com>
2025-01-24 09:51:28 +0100

    dodano obliaczaeni objetosci kuli

--- program.c
+++ program.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-float cuboid_volume(float a, float b,float h){
-    return(a*b*h);
-}
-int main() {
-    float a, b, h;
+float cuboid_volume(float a, float b, float h) {
 
-    printf("podaj dlugosc podstawy prostopadlosciana: ");
+return(a*b*h);
+
+}
+float ball_volume(float r)
+{
+    return((4.0/3.0)*3.1415)*(r*r*r);
+}
+
+int main()
+{
+    float a, b, h, r;
+    printf("Podaj promien kuli\n");
+    scanf("%f", &r);
+   /* printf("podaj dlugosc podstawy prostopadloscianu\n");
     scanf("%f", &a);
 
-
-    printf("podaj szerokosc podstawy prostopadlosciana: ");
+    printf("podaj szerokosc podstawy prostopadloscianu\n");
     scanf("%f", &b);
 
-
-    printf("podaj wysokosc prostopadlosciana: ");
+    printf("podaj wysokosc prostopadloscianu\n");
     scanf("%f", &h);
 
+    printf("\nObjetosc prostopadloscianu: %f", cuboid_volume(a,b,h)); */
+    printf("\nObjentosc kuli: %f", ball_volume(r));
 
-    printf("\Obientosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
-
-return(0);
+    return 0;
 }
